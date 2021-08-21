d68402bfe66706a8abcbe922c4b96a06699d0588
Alex <alex1031alex@yandex.ru>
2021-08-21 16:45:12 +0300

    1.13 Переделает компонент GameRules чтобы предотвратить клики по карте до начала игры

--- src/components/game-rules/game-rules.jsx
+++ src/components/game-rules/game-rules.jsx
@@ -5,19 +5,23 @@ const GameRules = (props) => {
   const {onStartButtonClick} = props;
 
   return <section className="popup game-rules">
-    <h2 className="popup__title">
-      Как хорошо Вы знаете Смоленскую область?
-    </h2>
-    <p className="popup__text">
-      Задача: найти на карте все районы Смоленской области
-    </p>
-    <p className="popup__text">
-      Найденные с первого раза окрашиваются в <span className="popup__text--green">зеленый</span> цвет, со второго - в <span className="popup__text--yellow">жёлтый</span>, с третьего - в <span className="popup__text--orange">оранжевый</span>.
-    </p>
-    <p className="popup__text">
-      После трех неверных попыток правильный район выделяется <span className="popup__text--crimson">малиновым</span> цветом, а после клика по нему, становится <span className="popup__text--red">красным</span>.
-    </p>
-    <button className="popup__btn" onClick={onStartButtonClick}>Начать тест!</button>
+    <div className="popup__inner">
+      <h2 className="popup__title">
+        Как хорошо Вы знаете Смоленскую область?
+      </h2>
+      <p className="popup__text">
+        Задача: найти на карте все районы Смоленской области
+      </p>
+      <p className="popup__text">
+        Найденные с первого раза окрашиваются в <span className="popup__text--green">зеленый</span> цвет, со второго - в <span className="popup__text--yellow">жёлтый</span>, с третьего - в <span className="popup__text--orange">оранжевый</span>.
+      </p>
+      <p className="popup__text">
+        После трех неверных попыток правильный район выделяется <span className="popup__text--crimson">малиновым</span> цветом, а после клика по нему, становится <span className="popup__text--red">красным</span>.
+      </p>
+      <button className="popup__btn" onClick={onStartButtonClick}
+      >Начать тест!</button>
+    </div>
+
   </section>;
 };
 
