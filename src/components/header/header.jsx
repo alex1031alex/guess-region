e21cefbbc2dd02cf4f3ec0a2538c7bc33ead9ca0
Alex <alex1031alex@yandex.ru>
2022-06-04 17:16:24 +0300

    correct logo path

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import sign from '../../img/sign.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__inner">
         <div className="header__logo">
-          <img src="img/sign.png" alt="Герб Смоленской области" width="42" height="51"/>
+          <img src={sign} alt="Герб Смоленской области" width="42" height="51"/>
         </div>
         <h1 className="header__title">Как хорошо Вы знаете Смоленскую область?
         </h1>
