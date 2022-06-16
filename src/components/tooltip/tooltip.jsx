9c1d3992566d3e09e70b5ca82d6f7018990455eb
Alex <alex1031alex@yandex.ru>
2022-06-16 19:25:46 +0300

    change classname

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -1,13 +1,12 @@
 import './tooltip.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import './tooltip.css';
 
 const Tooltip = (props) => {
   const {message, coordX, coordY} = props;
 
   return (
-    <div className="tooltip" style={{position: "absolute", left: coordX, top: coordY, backgroundColor: "#fff"}}>
+    <div className="a-tooltip" style={{position: "absolute", left: coordX, top: coordY, backgroundColor: "#fff"}}>
       <p className="tooltip__text">
         {message}
       </p>
