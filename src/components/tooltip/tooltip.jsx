e625fd02bc511d660851cf42583c6f4ed813834a
Alex <alex1031alex@yandex.ru>
2022-07-07 19:39:48 +0300

    style attribute was deleted

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -6,7 +6,7 @@ const Tooltip = (props) => {
   const {message, coordX, coordY} = props;
 
   return (
-    <div className="tooltip" style={{position: "absolute", left: coordX, top: coordY, backgroundColor: "#fff"}}>
+    <div className="tooltip">
       <p className="tooltip__text">
         {message}
       </p>
